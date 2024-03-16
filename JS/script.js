bd0ddc4170a7c67546a03e4bf8e252cc0d0245f2
Shawsaravanan <145187369+Shawsaravanan@users.noreply.github.com>
2024-03-16 13:40:03 +0530

    script.js

--- JS/script.js
+++ JS/script.js
@@ -24,42 +24,49 @@ $(document).ready(function () {
 Progress bars
 */
 $(document).ready(function () {
-    $("#progress-element").waypoint(function () {
-        $(".progress-bar").each(function () {
-            $(this).animate({
-                width: $(this).attr("aria-valuenow") + "%"
-            }, 1000);
+        $("#progress-element").waypoint(function () {
+            $(".progress-bar").each(function () {
+                $(this).animate({
+                    width: $(this).attr("aria-valuenow") + "%"
+                }, 3000);
+            });
+            this.destroy();
+        }, {
+            offset: 'bottom-in-view'
         });
-        this.destroy();
-    }, {
-        offset: 'bottom-in-view'
     });
 
+$('#start-button a').on('click', function (event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 2000, function () {
+            window.location.hash = hash;
+        });
+    }
 });
+function sendEmail(){
 
+	var email=document.getElementById("email").value;
+	var from_name=document.getElementById("name").value;
+	var message=document.getElementById("message").value;
 
+	var templateParams = {
+        email: email,
+        to_name: from_name,
+		message: message
+      };
 
+      emailjs.send('service_sn985lk', 'template_ctxj18o', templateParams)
+  .then(function(response) {
+     console.log('SUCCESS!', response.status, response.text);
+     window.alert("Sent successfully!");
+     
+  })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+     
+}
